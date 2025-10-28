9205d26ff0cc8b6cb1c52c986b29fcadcf729d92
110119076 <venkataped@gmail.com>
2025-10-28 10:10:22 +0530

    fix(animeRecommender): Create Retrieval QA and promptTemplate fixes

--- src/recommender.py
+++ src/recommender.py
@@ -1,23 +1,23 @@
-from langchain_community.chains import PebbloRetrievalQA
+from langchain_classic.chains.retrieval import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_groq import ChatGroq
 from src.prompt_template import get_anime_prompt
-
 class AnimeRecommender:
     def __init__(self, retriever, api_key: str, model_name: str):
         self.llm = ChatGroq(api_key=api_key, model=model_name, temperature=0)
         self.prompt = get_anime_prompt()
 
-        self.qa_chain = PebbloRetrievalQA.from_chain_type(
-            llm= self.llm,
-            chain_type="stuff",
-            retriever=retriever,
-            return_source_documents=True,
-            chain_type_kwargs={"prompt": self.prompt},
-            app_name="ANIME_RECOMMENDER",
-            description="Anime Recommender",
-            owner="Venkata Sai"
+        self.doc_chain = create_stuff_documents_chain(
+            llm = self.llm,
+            prompt = self.prompt,
+        )
+
+        self.qa_chain = create_retrieval_chain(
+            retriever = retriever,
+            combine_docs_chain = self.doc_chain,
         )
     
     def get_recommendation(self, query:str):
-        result = self.qa_chain({"query": query})
-        return result["result"]
+        result = self.qa_chain.invoke({"input": query})
+        print("result", result)
+        return result["answer"]
